ad11d0e198283bfce734b04a87c756851c85e92b
Libertad <lrivascedillo@gmail.com>
2024-09-03 13:06:08 -0600

    Se terminaron los pasos del 3 al 7

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ const txtNombre = document.getElementById("Name");
 const txtNumber = document.getElementById("Number");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+const contadorProductos = document.getElementById("contadorProductos");
+let productosTotal = document.getElementById("productosTotal");
+let precioTotal = document.getElementById("precioTotal");
+// bandera, al ser true permite agregar datos a la tabla
+let isValid = true;
+let contador = 0;
+let precio =0;
+let costoTotal = 0;
+let totalEnProductos=0;
+
 
 function validarCantidad(){
     if (txtNumber.value.length==0){
@@ -19,8 +31,11 @@ function validarCantidad(){
     }
 
     return true;
-}
+} //cierra función validarCantidad
 
+function getPrecio(){
+    return Math.round((Math.random()*10000))/100;
+} // cierra getPrecio
 
 
 
@@ -32,20 +47,49 @@ btnAgregar.addEventListener("click", function (event){
         txtNumber.style.border="";
         alertValidacionesTexto.innerHTML="";
         alertValidaciones.style.display="none";
+        isValid = true;
 // Validación del nombre del producto  
     if (txtNombre.value.length<3) {
         txtNombre.style.border="solid red medium";
         alertValidacionesTexto.innerHTML="El <strong> Nombre </strong> no es correcto. <br>"
-            alertValidaciones.style.display="block"
-    }
+        alertValidaciones.style.display="block"
+        isValid = false;
+    } //cierre validación del nombre
 // Validación de la cantidad
     if (! validarCantidad()){
         txtNumber.style.border="solid red medium";
         alertValidacionesTexto.innerHTML+="La <strong>cantidad</strong> no es correcta. <br>"
-            alertValidaciones.style.display="block"
-        return false;
-} //!validarCantidad
+        alertValidaciones.style.display="block"
+        isValid = false;
+    } //cierre validarCantidad
 
+    if (isValid){
+        contador ++;
+        precio = getPrecio();
+        let row =`<tr>
+                    <td>${contador}</td>
+                    <td>${txtNombre.value}</td>
+                    <td>${txtNumber.value}</td>
+                    <td>${precio}</td>
+        </tr>` ;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        costoTotal += precio * Number(txtNumber.value);
+        totalEnProductos += Number(txtNumber.value);
+        contadorProductos.innerText = contador;
+        productosTotal.innerText=totalEnProductos;
+        precioTotal.innerText= "$" + costoTotal.toFixed(2);
+        localStorage.setItem("contador", contador);
+        localStorage.setItem("totalEnProductos", totalEnProductos);
+        localStorage.setItem("costoTotal", costoTotal);
+
+
+
+        // para limpiar después de agregar
+        txtNombre.value="";
+        txtNumber.value="";
+        txtNombre.focus();
+        
+    }//cierre de isValid
     
 }); //Aquí termina el btnAgregar.addEventListener
 
@@ -60,4 +104,19 @@ txtNumber.addEventListener("blur", function(event){
 
 }); //Aquí termina el txtNumber.addEventListener
 
+window.addEventListener("load", function(){
+    if (this.localStorage.getItem("contador")!=null){
+        contador=Number(this.localStorage.getItem("contador"));
+    } // cierre de null 
 
+    if (this.localStorage.getItem("totalEnProductos")!=null){
+        totalEnProductos = Number(localStorage.getItem("totalEnProductos"));
+    } //cierre de null
+
+    if (this.localStorage.getItem("costoTotal")!=null){
+        costoTotal=Number(localStorage.getItem("costoTotal"));
+    } //cierre de null
+    contadorProductos.innerText = contador;
+    productosTotal.innerText=totalEnProductos;
+    precioTotal.innerText="$"+costoTotal.toFixed(2);
+});
